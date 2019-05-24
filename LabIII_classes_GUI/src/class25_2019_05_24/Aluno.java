164a4ea60e3c81dd4a55daaefd313bbcc57dfdf1
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-05-24 12:10:47 -0300

    Adding class25 MDI

---
+++ LabIII_classes_GUI/src/class25_2019_05_24/Aluno.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class25_2019_05_24;
+
+/**
+ *
+ * @author wesley
+ */
+public class Aluno {
+    
+    private String codigo;
+    private String nome;
+    private String curso;
+    
+    public Aluno() {
+        
+    }
+
+    public Aluno(String codigo, String nome, String curso) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.curso = curso;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
